b43538a4111430a60471b4215a19e5c5ac3e2e35
yeonfish6040 <yeonfish6040@gmail.com>
2023-02-07 19:13:35 +0900

    [안미정] - 서버 연동 확인용 hello 띄워보기

--- src/main/java/com/duboomom/memo/test/HelloController.java
+++ src/main/java/com/duboomom/memo/test/HelloController.java
@@ -1,5 +1,16 @@
 package com.duboomom.memo.test;
 
-public class HelloController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+public class HelloController {
+	
+	@ResponseBody
+	@GetMapping("/hello")
+	public String Hello() {
+		return "Hello world~~";
+	}
+	
 }
