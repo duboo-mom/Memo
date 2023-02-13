548bbe02165695539b739f911a9fc3fae5dfcc9f
yeonfish6040 <yeonfish6040@gmail.com>
2023-02-13 21:50:09 +0900

    [안미정] - 메모 작성 기능 - 메모 리스트 연결
    duboo-mom/Memo#4

---
+++ src/main/java/com/duboomom/memo/post/bo/PostBO.java
@@ -0,0 +1,20 @@
+package com.duboomom.memo.post.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.duboomom.memo.post.dao.PostDAO;
+
+@Service
+public class PostBO {
+
+	@Autowired
+	private PostDAO postDAO;
+	
+	public int addPost(int userId, String title, String content) {
+		
+		return postDAO.insertPost(userId, title, content);
+		
+	}
+	
+}
