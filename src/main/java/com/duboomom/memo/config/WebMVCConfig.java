78e4cb4fb4f214d23f32bb94d9fd4ab7256bc70b
yeonfish6040 <yeonfish6040@gmail.com>
2023-02-24 19:43:06 +0900

    [안미정] - 로그인 여부에 따라 접근 가능한 경로 설정
    duboo-mom/Memo#5

--- src/main/java/com/duboomom/memo/config/WebMVCConfig.java
+++ src/main/java/com/duboomom/memo/config/WebMVCConfig.java
@@ -1,20 +1,33 @@
 package com.duboomom.memo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.duboomom.memo.common.FileManagerService;
+import com.duboomom.memo.common.PermissionInterceptor;
 
 @Configuration
 public class WebMVCConfig implements WebMvcConfigurer {
 
+	@Autowired
+	private PermissionInterceptor interceptor;
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		
 		registry.addResourceHandler("/images/**")
 		.addResourceLocations("file:///" + FileManagerService.FILE_UPLOAD_PATH + "/");
 		
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(interceptor)
+		.addPathPatterns("/**")  // 인터셉터를 거쳐서 처리할 페이지의 url 규칙
+		.excludePathPatterns("/user/signout", "/static.**", "/images/**");
 	}
 	
 }
